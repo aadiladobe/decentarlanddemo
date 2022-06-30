8e232dab1858e904c133c16276f131930cbb61af
Aadil Khan <aadilk@adobe.com>
2022-07-01 03:04:43 +0530

    first commit v2

---
+++ src/game.ts
@@ -0,0 +1,95 @@
+import * as utils from '@dcl/ecs-scene-utils'
+import { Arissa } from './arissa'
+
+// Base
+const base = new Entity()
+base.addComponent(new GLTFShape('models/baseGrass.glb'))
+engine.addEntity(base)
+
+// Arissa
+const arissa = new Arissa(
+  new GLTFShape('models/arissa.glb'),
+  new Transform({
+    position: new Vector3(0, 0.05, -0.1),
+    scale: new Vector3(0, 0, 0)
+  })
+)
+arissa.setParent(Attachable.AVATAR)
+
+// Hide avatars
+const hideAvatarsEntity = new Entity()
+hideAvatarsEntity.addComponent(
+  new AvatarModifierArea({
+    area: { box: new Vector3(16, 4, 11) },
+    modifiers: [AvatarModifiers.HIDE_AVATARS]
+  })
+)
+hideAvatarsEntity.addComponent(
+  new Transform({ position: new Vector3(8, 2, 10.5) })
+)
+engine.addEntity(hideAvatarsEntity)
+
+// Create to show Arissa avatar
+hideAvatarsEntity.addComponent(
+  new utils.TriggerComponent(
+    new utils.TriggerBoxShape(new Vector3(16, 4, 11), Vector3.Zero()),
+    {
+      onCameraEnter: () => {
+        arissa.getComponent(Transform).scale.setAll(1)
+      },
+      onCameraExit: () => {
+        arissa.getComponent(Transform).scale.setAll(0)
+      }
+    }
+  )
+)
+
+// Check if player is moving
+const currentPosition = new Vector3()
+
+class CheckPlayerIsMovingSystem implements ISystem {
+  update() {
+    if (currentPosition.equals(Camera.instance.position)) {
+      arissa.playIdle()
+    } else {
+      currentPosition.copyFrom(Camera.instance.position)
+      arissa.playRunning()
+    }
+  }
+}
+
+engine.addSystem(new CheckPlayerIsMovingSystem())
+// #1
+const myVideoClip = new VideoClip(
+  'https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875'
+)
+
+// #2
+const myVideoTexture = new VideoTexture(myVideoClip)
+
+// #3
+const myMaterial = new Material()
+myMaterial.albedoTexture = myVideoTexture
+myMaterial.roughness = 1
+myMaterial.specularIntensity = 0
+myMaterial.metallic = 0
+
+
+// #4
+const screen = new Entity()
+screen.addComponent(new PlaneShape())
+screen.addComponent(
+  new Transform({
+    position: new Vector3(8, 1, 8),
+  })
+)
+screen.addComponent(myMaterial)
+screen.addComponent(
+  new OnPointerDown(() => {
+    myVideoTexture.playing = !myVideoTexture.playing
+  })
+)
+engine.addEntity(screen)
+
+// #5
+myVideoTexture.play()
